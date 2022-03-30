6aff3751e58edd8073269ba59aad28fae271f76d
vaishukk <khismatrao.v@northeastern.edu>
2022-03-30 00:07:22 -0400

    code deployment files added

---
+++ codedeploy/afterInstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sudo systemctl stop tomcat.service
+sudo systemctl stop webapp.service
+sudo systemctl stop amazon-cloudwatch-agent.service
+
+#removing previous build ROOT folder
+#sudo rm -rf /opt/tomcat/webapps/ROOT
+
+#sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
+
+# cleanup log files
+#sudo rm -rf /opt/tomcat/logs/catalina*
+#sudo rm -rf /opt/tomcat/logs/*.log
+#sudo rm -rf /opt/tomcat/logs/*.txt
